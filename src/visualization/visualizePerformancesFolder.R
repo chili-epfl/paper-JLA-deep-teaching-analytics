8e5e61b51747205f4a9da25dfa166dd5b14f50e3
Luis P <lprisan@hotmail.com>
2017-05-15 09:35:26 +0200

    Polished performance visualization script

--- src/visualization/visualizePerformancesFolder.R
+++ src/visualization/visualizePerformancesFolder.R
@@ -5,7 +5,6 @@
 # NB: It requires quite a few data files from different schemas for the graphs to work correctly
 
 library(ggplot2)
-library(feather)
 
 args <- commandArgs(trailingOnly=FALSE)
 # Get the --file argument to get the current script location
@@ -20,11 +19,13 @@ if(length(args)!=1){
 }
 
 strdir <- args[1]
-
+#strdir <- "../models/"
 
 # Find and read the RData files
 # TODO: for Python stuff, store the data using feather-format!
 files <- list.files(path = strdir, pattern = "\\.RData$", ignore.case = T)
+
+nor=F
 
 df <- data.frame()
 if(length(files)>0){
@@ -35,13 +36,47 @@ if(length(files)>0){
     #print(out)
     data <- data.frame(label=label, auc=auc, kappa=cm$overall['Kappa'], acc=cm$overall['Accuracy'], f1=f1)
     
-    if(nrow(df)==0) df <- data
-    else df <- rbind(df,data)
+    if(nrow(df)==0){
+      df <- data 
+    }
+    else{
+      df <- rbind(df,data) 
+    }
   }
   
 }else{
-  stop("No data files in the specified location!")
+  print("No R data files in the specified location!")
+  nor=T
 }
+
+# Same thing for the python performance files
+files <- list.files(path = strdir, pattern = "\\.perf.csv$", ignore.case = T)
+if(length(files)>0){
+  
+  for(file in files){
+    
+    out <- read.csv(paste(strdir,file,sep=.Platform$file.sep))
+    #print(out)
+    data <- data.frame(label=out$label, auc=out$auc, kappa=out$kappa, acc=out$acc, f1=out$f1)
+    
+    if(nrow(df)==0){
+      df <- data 
+    }
+    else{
+      df <- rbind(df,data) 
+    }
+  }
+  
+}else{
+  print("No Python data files in the specified location!")
+  if(nor){
+    stop("No data files to analyze!")
+  }
+}
+
+
+
+
 
 # Add some group variables for each kind of target, sources, validation model
 df$model <- factor(sapply(as.character(df$label), FUN=function(x){unlist(strsplit(x, split = "_", fixed = T))[1]}))
@@ -58,35 +93,35 @@ setwd(path)
 # Graphs for Activity
 gg <- ggplot(df[df$target=='Activity',], aes(x=modelsources, y=acc)) + 
   geom_boxplot(aes(fill=model)) + facet_grid(validation ~ modeltype) +
-  theme_bw() + ggtitle("Accuracy, Activity")
+  theme_bw() + ggtitle("Accuracy, Activity") + geom_jitter(alpha=0.1) + coord_flip()
 gg
 gg <- ggplot(df[df$target=='Activity',], aes(x=modelsources, y=kappa)) + 
   geom_boxplot(aes(fill=model)) + facet_grid(validation ~ modeltype) +
-  theme_bw() + ggtitle("Kappa, Activity")
+  theme_bw() + ggtitle("Kappa, Activity")  + geom_jitter(alpha=0.1) + coord_flip()
 gg
 gg <- ggplot(df[df$target=='Activity',], aes(x=modelsources, y=auc)) + 
   geom_boxplot(aes(fill=model)) + facet_grid(validation ~ modeltype) +
-  theme_bw() + ggtitle("AUC, Activity")
+  theme_bw() + ggtitle("AUC, Activity") + geom_jitter(alpha=0.1) + coord_flip()
 gg
 gg <- ggplot(df[df$target=='Activity',], aes(x=modelsources, y=f1)) + 
   geom_boxplot(aes(fill=model)) + facet_grid(validation ~ modeltype) +
-  theme_bw() + ggtitle("F1 Score, Activity")
+  theme_bw() + ggtitle("F1 Score, Activity") + geom_jitter(alpha=0.1) + coord_flip()
 gg
 
 # Graphs for Social
 gg <- ggplot(df[df$target=='Social',], aes(x=modelsources, y=acc)) + 
   geom_boxplot(aes(fill=model)) + facet_grid(validation ~ modeltype) +
-  theme_bw() + ggtitle("Accuracy, Social")
+  theme_bw() + ggtitle("Accuracy, Social") + geom_jitter(alpha=0.1) + coord_flip()
 gg
 gg <- ggplot(df[df$target=='Social',], aes(x=modelsources, y=kappa)) + 
   geom_boxplot(aes(fill=model)) + facet_grid(validation ~ modeltype) +
-  theme_bw() + ggtitle("Kappa, Social")
+  theme_bw() + ggtitle("Kappa, Social") + geom_jitter(alpha=0.1) + coord_flip()
 gg
 gg <- ggplot(df[df$target=='Social',], aes(x=modelsources, y=auc)) + 
   geom_boxplot(aes(fill=model)) + facet_grid(validation ~ modeltype) +
-  theme_bw() + ggtitle("AUC, Social")
+  theme_bw() + ggtitle("AUC, Social") + geom_jitter(alpha=0.1) + coord_flip()
 gg
 gg <- ggplot(df[df$target=='Social',], aes(x=modelsources, y=f1)) + 
   geom_boxplot(aes(fill=model)) + facet_grid(validation ~ modeltype) +
-  theme_bw() + ggtitle("F1 Score, Social")
+  theme_bw() + ggtitle("F1 Score, Social") + geom_jitter(alpha=0.1) + coord_flip()
 gg
